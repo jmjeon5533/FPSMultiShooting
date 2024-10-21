5375670c2fc5debc1766407bcbf2480109102f54
jmjeon5533 <jmjeon5533@gmail.com>
2024-10-21 18:26:32 +0900

    Fusion Setting

---
+++ fusionShooting/Assets/Scripts/Network/NetworkRunnerHandler.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fusion;
+using Fusion.Sockets;
+using UnityEngine.SceneManagement;
+using System.Threading.Tasks;
+using System;
+using System.Linq;
+
+public class NetworkRunnerHandler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+}
